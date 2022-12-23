5da3141c8da3b33f108e0dea9d5b1cee5741005e
Вадим <itamiomw@gmail.com>
2022-12-23 11:40:17 +0300

    set up api service

---
+++ app/src/main/java/com/example/stockmarket/data/remote/StockMarketApi.kt
@@ -0,0 +1,53 @@
+package com.example.stockmarket.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockMarketApi {
+
+    @GET(QUERY_COMPANY_LIST)
+    suspend fun getListings(
+        @Query(API_KEY_PARAM) apikey: String = API_KEY_VALUE,
+    ): ResponseBody
+
+
+    @GET(QUERY_INTRADAY_INFO)
+    suspend fun getIntradayInfo(
+        @Query(SYMBOL_PARAM) symbol: String,
+        @Query(API_KEY_PARAM) apikey: String = API_KEY_VALUE,
+    ): ResponseBody
+
+
+    @GET(QUERY_COMPANY_INFO)
+    suspend fun getCompanyInfo(
+        @Query(SYMBOL_PARAM) symbol: String,
+        @Query(API_KEY_PARAM) apikey: String = API_KEY_VALUE,
+    ): ResponseBody
+
+
+    companion object {
+
+        //BASE URL OF THE API
+        const val BASE_URL = "https://www.alphavantage.co/"
+
+        //MY FREE API KEY
+        private const val API_KEY_VALUE = "I484NN6CI8GF31DB"
+
+        //PARAM FOR API KEY
+        private const val API_KEY_PARAM = "apikey"
+
+        //PARAM FOR SYMBOL
+        private const val SYMBOL_PARAM = "symbol"
+
+        //QUERY FOR LIST OF COMPANIES
+        private const val QUERY_COMPANY_LIST = "query?function=LISTING_STATUS"
+
+        //QUERY FOR INTRADAY INFO
+        private const val QUERY_INTRADAY_INFO = "query?function=TIME_SERIES_INTRADAY&interval=60min"
+
+        //QUERY FOR COMPANY INFO
+        private const val QUERY_COMPANY_INFO = "query?function=OVERVIEW"
+    }
+
+}
---
+++ app/src/main/java/com/example/stockmarket/data/remote/dto/IntradayDto.kt
@@ -0,0 +1,6 @@
+package com.example.stockmarket.data.remote.dto
+
+data class IntradayDto(
+    val timeStamp: String,
+    val close: Double
+)
